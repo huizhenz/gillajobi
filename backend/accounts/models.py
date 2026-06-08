577a249228975080b4ca5a7a051a5edd53cfdc2b
wjdalswl-airair <jmj20021030@gmail.com>
2026-06-08 16:33:04 +0900

    feat: UserModel 속성 추가, ProfileModel 설계

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,8 +2,28 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+GENDER_CHOICES = [
+    ('M', '남성'),
+    ('F', '여성'),
+]
+
 class User(AbstractUser):
-    nickname = models.CharField(max_length=125, unique=True)
+    nickname = models.CharField(
+        max_length=125,
+        unique=True
+    )
+
+    email = models.EmailField(
+        unique=True
+    )
+
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+    )
+
+    birth = models.DateField()
+
     profile_image = models.ImageField(
         upload_to='profiles/',
         blank=True,
@@ -12,3 +32,49 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+
+    education = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    certification = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    experience = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    language = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    preferred_location = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    preferred_position = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    desired_salary = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
