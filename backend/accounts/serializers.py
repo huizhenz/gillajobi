e71a3cced3712bb6b51cc791205ffe7a97e4a522
wjdalswl-airair <jmj20021030@gmail.com>
2026-06-17 23:12:01 +0900

    feat: dj-rest-auth를 사용하여 seirializer 수정

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,24 +1,34 @@
 from rest_framework import serializers
 from .models import User, Profile
+from dj_rest_auth.registration.serializers import RegisterSerializer
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(RegisterSerializer):
+    nickname = serializers.CharField()
+    gender = serializers.ChoiceField(choices=User._meta.get_field('gender').choices)
+    birth = serializers.DateField()
+    profile_image = serializers.ImageField(required=False, allow_null=True)
 
-    class Meta:
-        model = User
-        fields = (
-            'id',
-            'username',
-            'password',
-            'email',
-            'nickname',
-            'gender',
-            'birth',
-            'profile_image',
-        )
-        extra_kwargs = {
-            'password': {'write_only': True},
-        }
+    def get_cleaned_data(self):
+        data = super().get_cleaned_data()
+        data['nickname'] = self.validated_data.get('nickname')
+        data['gender'] = self.validated_data.get('gender')
+        data['birth'] = self.validated_data.get('birth')
+        data['profile_image'] = self.validated_data.get('profile_image')
+        return data
+    
+    def save(self, request):
+        user = super().save(request)
+
+        user.nickname = self.validated_data.get('nickname')
+        user.gender = self.validated_data.get('gender')
+        user.birth = self.validated_data.get('birth')
+        user.profile_image = self.validated_data.get('profile_image')
+        user.save()
+
+        Profile.objects.create(user=user)
+
+        return user
 
 
 class ProfileSerializer(serializers.ModelSerializer):
