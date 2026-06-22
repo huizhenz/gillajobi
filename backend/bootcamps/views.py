189788ab17b1a1305285eea7db014d90c6ab5fb3
minji <jmj20021030@gmail.com>
2026-06-22 09:47:00 +0900

    feat: bootcamp 크롤링 및 조회 코드 작성

--- backend/bootcamps/views.py
+++ backend/bootcamps/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import (
+    Region,
+    Skill,
+    Bootcamp
+)
+from .serializers import (
+    RegionSerializer,
+    SkillSerializer,
+    BootcampSerializer,
+)
+
+from .service import fetch_bootcamps_service
+from django.conf import settings
+
+
+@api_view(['GET'])
+def bootcamps_list(request):
+    bootcamps = Bootcamp.objects.all()
+    serializer = BootcampSerializer(bootcamps, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def fetch_bootcamps(request):
+
+    result = fetch_bootcamps_service()
+
+    return Response(result)
