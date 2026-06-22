20f3728f88766f597eca6db5f062c07f7fd6b611
Hyejin Jang <hyeee513@naver.com>
2026-06-22 23:46:11 +0900

    feat: Article 모델 필드(label) 추가

--- backend/community/models.py
+++ backend/community/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.conf import settings
-from category.models import Category
+from category.models import Label
 
 # Create your models here.
 class Article(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    label = models.ForeignKey(
+        Label,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='articles'
+    )
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
